6a1801cbfad299eb59fa99b68f6f805032ac96ce
kelvinwahyudi <gokelvin4@gmail.com>
2024-08-26 17:03:10 +0700

    local kelvin

--- resources/views/register/pages/ranap/index.blade.php
+++ resources/views/register/pages/ranap/index.blade.php
@@ -47,9 +47,9 @@
                   <h3 class="card-title">Pendaftaraan Rawat Inap </h3>
                 </div>
                 <div class="col d-flex justify-content-end gap-1">
-                  <!-- <a href="{{ route('register.ranap.create') }}" class="btn btn-success btn-sm ml-auto">
+                  <a href="{{ route('register.ranap.create') }}" class="btn btn-success btn-sm ml-auto">
                 Tambah Data
-                </a> -->
+                </a>
                   <a href="{{route('register.vclaim')}}">
                     <button onclick="{{ route('register.vclaim') }}" class="btn btn-warning radius ml-3"><i class="fas fa-user-check"></i> Vclaim Manual</button>
                   </a>
