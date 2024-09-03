719ce100b40cb09e4a7b8be0ca9c21893884d4be
Unique Constraints <hdytamri@gmail.com>
2024-09-03 21:32:43 +0700

    more update

---
+++ database/migrations/2024_09_03_145424_amr_modify_edukasi_drop_foreign_key.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AmrModifyEdukasiDropForeignKey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rs_edukasi_pasien', function (Blueprint $table) {
+            // Gantilah 'foreign_key_name' dengan nama foreign key yang ingin Anda hapus
+            $table->dropColumn('user_id');
+
+            $table->integer('user_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        
+    }
+}
