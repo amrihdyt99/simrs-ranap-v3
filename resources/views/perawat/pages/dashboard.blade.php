e2b5b488cd097edceafbee13b566c59024b2d621
Fadhil Sa'adat <102606438+Fadhilsaadat@users.noreply.github.com>
2024-09-29 01:24:06 +0700

    update lantai perawat

--- resources/views/perawat/pages/dashboard.blade.php
+++ resources/views/perawat/pages/dashboard.blade.php
@@ -4,97 +4,58 @@
 <div class="card">
   <div class="card-header p-2">
     <ul class="nav nav-pills">
-
-      <li class="nav-item text-sm"><a class="nav-link active" href="#my-patient" data-toggle="tab">My Patient</a>
-      </li>
+      <li class="nav-item text-sm"><a class="nav-link active" href="#my-patient" data-toggle="tab">My Patient</a></li>
     </ul>
   </div><!-- /.card-header -->
   <div class="card-body">
     <div class="tab-content">
-      {{--<div class="active tab-pane" id="my-area">
-                <div class="card">
-                    <!-- /.card-header -->
-                    <div class="card-body">
-                        <table id="example2" class="table table-bordered table-hover">
-                            <thead>
-                                <tr>
-                                    <th class="text-sm">Reg No</th>
-                                    <th class="text-sm">Patient's Name</th>
-                                    <th class="text-sm">MRN</th>
-                                    <th class="text-sm">Doctor's Name</th>
-                                    <th class="text-sm">Loc</th>
-                                    <th class="text-sm">Payer</th>
-                                    <th class="text-sm">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($myArea as $item)
-                                <tr>
-                                    <td class="text-sm">{{$item->reg_no}}</td>
-      <td class="text-sm">{{$item->PatientName}}</td>
-      <td class="text-sm">{{$item->reg_medrec}}</td>
-      </td>
-      <td class="text-sm">{{$item->ParamedicName}}</td>
-      <td class="text-sm">{{$item->nama_ruangan}}</td>
-      <td class="text-sm">{{$item->reg_cara_bayar}}</td>
-      <td class="text-sm">
-        <form action="" method="">
-          <button type="submit" class="btn btn-sm btn-outline-primary"><i class="mr-2 fa fa-clipboard-check"></i>Ambil
-          </button>
-          <a href="{{route('perawat.patient.detail',['reg_no'=>$item->reg_no])}}" target="_blank" class="btn btn-sm btn-outline-success"><i class="mr-2 fa fa-clipboard-check"></i>Detail</a>
-        </form>
-      </td>
-      </tr>
-      @endforeach
-      </tbody>
-      </table>
+      <div class="form-row">
+        <div class="form-group col-sm-12 col-md-6 col-lg-4">
+          <p class="m-0">Filter Ruangan *</p>
+          <select id="nama_ruangan" class="deteksi_change form-control">
+            <option value="">-- SEMUA --</option>
+          </select>
+        </div>
+        <div class="form-group col-sm-12 col-md-6 col-lg-4">
+          <p class="m-0">Tanggal Registrasi *</p>
+          <input name="reg_tgl" id="reg_tgl" autocomplete="off" type="text"
+            class="deteksi_change_dp form-control" onchange="dt_dst()" data-toggle="datetimepicker">
+        </div>
+        <div class="form-group col-sm-12">
+          <button type="button" id="refresh" class="btn btn-secondary btn-sm">Reset Filter</button>
+        </div>
+      </div>
+
+      <div class="active tab-pane" id="my-patient">
+        <div class="card">
+          <div class="card-body">
+            <div id="sini_table_patient"></div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Add this button to trigger the modal -->
+      <!-- <button class="btn btn-secondary" onclick="mod_pilih_ruang()" id="title_ruang">LANTAI PASIEN</button> -->
+
+      <div class="modal" id="modal_pil" tabindex="-1" data-toggle="modal" data-backdrop="static" data-keyboard="false">
+        <div class="modal-dialog modal-md">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Modal LANTAI PASIEN</h5>
+            </div>
+            <div class="modal-body">
+              <div class="form-group">
+                <label for="">Pilih LANTAI PASIEN</label>
+                <select name="pilru" id="pilru" class="form-control select2-mod" style="width: 100%;" onclick="list_ruang()" onchange="filter_ruang()"></select>
+              </div>
+              <button type="button" onclick="$('#modal_pil').modal('hide')" class="btn btn-secondary float-right" aria-label="Close">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
-    <!-- /.card-body -->
   </div>
-</div>--}}
-
-<div class="form-row">
-  <div class="form-group col-sm-12 col-md-6 col-lg-4">
-    <p class="m-0">Filter Ruangan *</p>
-    <select id="nama_ruangan" class="deteksi_change form-control">
-      <option value="">-- SEMUA --</option>
-    </select>
-  </div>
-  <div class="form-group col-sm-12 col-md-6 col-lg-4">
-    <p class="m-0">Tanggal Registrasi *</p>
-    <input name="reg_tgl" id="reg_tgl" autocomplete="off" type="text"
-      class="deteksi_change_dp form-control" onchange="dt_dst()" data-toggle="datetimepicker">
-  </div>
-  <div class="form-group col-sm-12">
-    <button type="button" id="refresh" class="btn btn-secondary btn-sm">Reset Filter</button>
-  </div>
-</div>
-
-<div class="active tab-pane" id="my-patient">
-  <div class="card">
-    <!-- /.card-header -->
-    <div class="card-body">
-      <table id="example1" class="w-100 table table-bordered table-hover">
-        <thead>
-          <tr>
-            <th class="text-sm">Reg No</th>
-            <th class="text-sm">Tanggal Registrasi</th>
-            <th class="text-sm">Patient's Name</th>
-            <th class="text-sm">MRN</th>
-            <th class="text-sm">Doctor's Name</th>
-            <th class="text-sm">Loc</th>
-            <th class="text-sm">Payer</th>
-            <th class="text-sm">Action</th>
-          </tr>
-        </thead>
-
-      </table>
-    </div>
-    <!-- /.card-body -->
-  </div>
-</div>
-</div>
-</div>
 </div>
 @endsection
 
@@ -102,101 +63,100 @@
 <!-- Page specific script -->
 <script>
   $(function() {
-    load_data();
     neko_datepicker("reg_tgl");
-  });
-
-  $(function() {
     neko_select2_init(`{{ route('nyaa_universal.select2.m_ruangan_baru_all') }}`, 'nama_ruangan');
-  })
-
-  $(function() {
+    
     $(".deteksi_change").on("change", function() {
       dt_dst();
     });
     $("#refresh").on("click", function() {
       dt_dst('reset');
     });
+
+    const storedUserId = localStorage.getItem('user_id');
+    const currentUserId = '{{ auth()->id() }}';
+
+    if (storedUserId === currentUserId && localStorage.getItem('pilihruang')) {
+      const pilihruang = localStorage.getItem('pilihruang').split(',');
+      load_data(pilihruang[0]);
+      $('#title_ruang').text(pilihruang[1]);
+    } else {
+      // Clear localStorage if user changed
+      localStorage.removeItem('pilihruang');
+      localStorage.setItem('user_id', currentUserId);
+      mod_pilih_ruang();
+    }
+
+    $('.select2-mod').select2({
+      dropdownParent: $("#modal_pil")
+    });
+
+    list_ruang()
   });
 
   function dt_dst(reset = null) {
     if (reset === 'reset') {
       $('#nama_ruangan').val('').trigger('change');
+      $('#reg_tgl').val('');
     }
-    neko_refresh_datatable('example1');
+    load_data($('#nama_ruangan').val());
   }
 
+  function mod_pilih_ruang() {
+    $('#modal_pil').modal('show')
+    list_ruang()
+  }
 
-  function load_data() {
-    $('#example1').DataTable({
-      processing: true,
-      serverSide: true,
-      responsive: true,
-      scrollX: true,
-      lengthMenu: [10, 25, 50, 100, 200, 500],
-      ajax: {
-        url: "{{ url()->current() }}",
-        type: "POST",
-        headers: {
-          "X-HTTP-Method-Override": "GET"
-        },
-        data: {
-          'nama_ruangan': function() {
-            return $("#nama_ruangan").val()
-          },
-          'reg_tgl': function() {
-            return $("#reg_tgl").val()
-          },
-        },
+  function list_ruang() {
+    $.ajax({
+      url: "{{url('')}}/master/aksesRuangan/data",
+      type: "get",
+      data: {
+        params: [
+          {key: 'ParamedicCode', value: $user_perawat_}
+        ]
       },
-      columns: [{
-          data: "reg_no",
-          name: "m_registrasi.reg_no",
-          orderable: true,
-          searchable: true,
-        },
-        {
-          data: "reg_tgl",
-          name: "m_registrasi.reg_tgl",
-          orderable: true,
-          searchable: true,
-        },
-        {
-          data: "PatientName",
-          name: "m_pasien.PatientName",
-          orderable: true,
-          searchable: true,
-        },
-        {
-          data: "reg_medrec",
-          name: "m_registrasi.reg_medrec",
-          orderable: true,
-          searchable: true,
-        },
-        {
-          data: "ParamedicName",
-          name: "m_paramedis.ParamedicName",
-          orderable: true,
-          searchable: true,
-        },
-        {
-          data: "RoomName",
-          name: "m_ruangan.RoomName",
-          orderable: true,
-          searchable: true,
-        },
-        {
-          data: "reg_cara_bayar",
-          name: "m_registrasi.reg_cara_bayar",
-          orderable: true,
-          searchable: true,
-        },
-        {
-          data: "aksi_data",
-          orderable: false,
-          searchable: false,
-        },
-      ],
+      success: function (r) {
+        var opt = '<option value="x,x" >Pilih Ruang</option>'
+        $.each(r, function(index, row) {
+          $.each(row.ParamedicAccessRoom, function(sub_i, sub_item){
+            opt += '<option value="' + sub_item.ServiceId +','+sub_item.ServiceName +'">' + sub_item.ServiceName + '</option>'
+          })
+        })
+        $('#pilru').html(opt)
+      }
+    })
+  }
+
+  function filter_ruang(){
+    let isi = $('#pilru').val()
+    const arr = isi.split(",")
+    if (arr[1] == 'x') {
+      alert('Pilih ruangan valid')
+    } else {
+      load_data(arr[0])
+      $('#title_ruang').text(arr[1])
+      $('#modal_pil').modal('hide')
+      localStorage.setItem('pilihruang', isi);
+    }
+  }
+
+  function load_data(ruang){
+    $.ajax({
+      type: "get",
+      url: "{{url('perawat/data')}}/" + ruang,
+      data: {
+        nama_ruangan: ruang,
+        reg_tgl: $('#reg_tgl').val()
+      },
+      dataType: "html",
+      success: function (r) {
+        $('#sini_table_patient').html(r)
+        $('#example1').DataTable({
+          processing: true,
+          responsive: true,
+        });
+      }
     });
   }
 </script>
