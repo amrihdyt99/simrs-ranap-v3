c89c05f949f55efc3cc91bcb927d7c3152308c8a
Rafli Agil Caesar <rafliagil17@gmail.com>
2024-10-07 02:20:36 +0700

    tambah physician team di dokter

--- routes/api.php
+++ routes/api.php
@@ -153,6 +153,8 @@ Route::post('addCathlabTimeOut', [\App\Http\Controllers\NewPerawat\NewNursingCon
 Route::post('addCathlabSignOut', [\App\Http\Controllers\NewPerawat\NewNursingController::class, 'simpan_cathlab_signout'])->name('add.cathlab_sign_out');
 Route::post('pemeriksaan_bayi', [\App\Http\Controllers\NewPerawat\NewNursingController::class, 'simpan_pemeriksaan_bayi'])->name('add.pemeriksaan_bayi');
 Route::get('getdokter', [App\Http\Controllers\NewPerawat\NewNursingController::class, 'getPhysician'])->name('get.dokter');
+Route::post('addphysicianteamDokter', [\App\Http\Controllers\NewDokter\PhysicianTeamController::class, 'addPhysicianTeamDokter'])->name('add.physicianteam.dokter');
+Route::delete('/deletePhysicianTeamDokter/{id}', [\App\Http\Controllers\NewDokter\PhysicianTeamController::class, 'deletePhysicianTeamDokter'])->name('delete.physicianteam.dokter');
 Route::post('addphysicianteam', [\App\Http\Controllers\NewPerawat\NewNursingController::class, 'addPhysicianTeam'])->name('add.physicianteam');
 Route::post('getphysicianteam', [\App\Http\Controllers\NewPerawat\NewNursingController::class, 'getPhysicianTeam'])->name('get.physicianteam');
 Route::post('addTindakanIntra', [App\Http\Controllers\NewPerawat\NewNursingController::class, 'addTindakanIntra'])->name('add.tindakan.intra');
