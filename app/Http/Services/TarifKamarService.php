e70d55a8c9da0dde4556fd14597ebf04e0861210
izmi.komar <komar.izmi@gmail.com>
2024-10-18 19:49:54 +0700

    Refactor TarifKamarService to use 'rute' instead of 'rute_obat' for medication route

--- app/Http/Services/TarifKamarService.php
+++ app/Http/Services/TarifKamarService.php
@@ -75,7 +75,7 @@ class TarifKamarService
             'status_kirim' => $data['status_kirim'] ?? null,
             'catatan_dokter' => $data['catatan_dokter'] ?? null,
             'bentuk_obat' => $data['bentuk_obat'] ?? null,
-            'rute_obat' => $data['rute_obat'] ?? null,
+            'rute' => $data['rute'] ?? null,
             'obat_pulang' => $data['obat_pulang'] ?? null,
             'created_at' => date('Y-m-d H:i:s')
         ];
