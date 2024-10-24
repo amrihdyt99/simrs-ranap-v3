ccfc766b5b3da24417d30bdd50b08dc5165ddf18
Unique Constraints <hdytamri@gmail.com>
2024-10-24 22:08:05 +0700

    more Update - Dokter

--- resources/views/dokter/pages/table.blade.php
+++ resources/views/dokter/pages/table.blade.php
@@ -21,9 +21,11 @@
                     Cara Bayar : {{$row->reg_cara_bayar}} <br><br>
                     Lokasi : <b>{{$row->RoomName}} / {{$row->ServiceUnitName}}</b> <br><br>
                     Role PPA :  
-                    @if (isset($row->physician_team_role))
-                        @foreach (json_decode($row->physician_team_role) as $item)
-                            <div class="badge badge-info">{{$item}}</div>
+                    @if (!empty($row->physicianTeam))
+                        @foreach($row->physicianTeam as $team_role)
+                            @if ($team_role->kode_dokter == auth()->user()->dokter_id)
+                                <div class="badge badge-info">{{$team_role->kategori}}</div>
+                            @endif
                         @endforeach
                     @else
                         <div class="badge badge-info">DPJP</div>
@@ -32,8 +34,8 @@
                 <td><b>{{$row->PatientName}}</b></td>
                 <td>{{$row->ParamedicName}}</td>
                 <td>
-                    @foreach(explode('|', $row->physician_team) as $team_member)
-                        <span style="margin-right: 5px; margin-bottom: 5px; white-space: nowrap; font-size: 0.875rem;" class="badge badge-primary">{{ trim($team_member) }}</span><br>
+                    @foreach($row->physicianTeam as $team_member)
+                        <span style="margin-right: 5px; margin-bottom: 5px; white-space: nowrap; font-size: 0.875rem;" class="badge badge-primary">{{ trim($team_member->ParamedicName) }}</span><br>
                     @endforeach
                 </td>
                 <td>
@@ -42,16 +44,6 @@
                             <i class="mr-2 fa fa-share-square"></i>Ambil Alih
                         </button>
                     @else
-                        @php
-                            $isKonsul = isset($row->physician_team_role) && in_array('Konsul', json_decode($row->physician_team_role));
-                        @endphp
-                        
-                        @if ($isKonsul)
-                            <div class="alert alert-warning mb-2" role="alert">
-                                <i class="fa fa-exclamation-triangle mr-2"></i>Pasien memerlukan konsultasi
-                            </div>
-                        @endif
-                        
                         <a href="{{route('dokter.patient.summary',['patient'=>$row->reg_no])}}" class="mb-1 btn btn-sm btn-outline-primary">
                             <i class="mr-2 fa fa-clipboard-check"></i>Periksa
                         </a><br>
@@ -62,6 +54,16 @@
                             <i class="mr-2 fa fa-times"></i>Batal
                         </button>
                     @endif
+                    
+                    <br><br>
+                    @foreach($row->physicianTeam as $team_role_alert)
+                        @if ($team_role_alert->kode_dokter == auth()->user()->dokter_id && $team_role_alert->kategori == 'Konsul')
+                            <div class="alert alert-warning mb-2" role="alert">
+                                <i class="fa fa-exclamation-triangle mr-2"></i>
+                                Pasien memerlukan konsultasi
+                            </div>
+                        @endif
+                    @endforeach
                 </td>
             </tr>
             @endforeach
